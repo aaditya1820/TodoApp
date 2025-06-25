e2a85cbc9bb5201683f0380f0d5e514137665a5e
Aaditya Patel <aadimanishajitu1122@gmail.com>
2025-06-25 20:54:32 +0530

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound";
-import { Toaster } from "react-hot-toast";
-function App() {
+
+function ProtectedRoute({ children }) {
   const token = localStorage.getItem("jwt");
+  return token ? children : <Navigate to="/login" />;
+}
+
+function App() {
   return (
     <div>
       <Routes>
-        <Route
-          path="/"
-          element={token ? <Home /> : <Navigate to="/" />}    
-        />
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Toaster />
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 }
